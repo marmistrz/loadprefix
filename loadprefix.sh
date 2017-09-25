a286afbaeb88309b27a131a59d15d80564284573
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2017-09-25 13:29:03 +0200

    Check if any prefixes have been added

--- loadprefix.sh
+++ loadprefix.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-for prefix in "$@"; do
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [prefixes...]"
+fi
+
+for p in "$@"; do
     if [ ! -d "$prefix" ]; then
         echo "Prefix $prefix doesn't exist!" 1>&2
         echo "Aborting" 1>&2
+        exit 1
     fi
 done
 
-for prefix in "$@"; do
+for p in "$@"; do
+    prefix=$(readlink -f "$p")  # resolve the full path
     export PATH="$prefix/bin:$PATH"
     export LD_LIBRARY_PATH="$prefix/lib:$LD_LIBRARY_PATH"
     export LIBRARY_PATH="$prefix/lib:$LIBRARY_PATH"
